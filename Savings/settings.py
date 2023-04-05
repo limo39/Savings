c4bf20bbda4abd6084ab13cc28d9da8b434219cd
LiMo Kiprono <94519855+limo39@users.noreply.github.com>
2023-04-05 23:30:52 +0300

    Update settings.py

--- Savings/settings.py
+++ Savings/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2qt5ulo1_lz-0n%aet^q&jk4nlh2nk2^ewrrg_4$4w*bw)&)=$'
+SECRET_KEY = ' '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
